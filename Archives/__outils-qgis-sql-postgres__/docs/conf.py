837850b87983a391b2a353e6749fae117c52128c
Nicolas Kulpinski <82100129+nkulpinski@users.noreply.github.com>
2025-12-11 08:50:34 +0100

    Update conf.py

--- Archives/__outils-qgis-sql-postgres__/docs/conf.py
+++ Archives/__outils-qgis-sql-postgres__/docs/conf.py
@@ -63,7 +63,7 @@ html_theme_options = {
             # Label for this link
             "name": "GitHub",
             # URL where the link will redirect
-            "url": "https://github.com/GT-CNIG-DDU-team/SG5-SYMBOLISATION",  # required
+            "url": "https://github.com/GT-CNIG-DDU-team/SG5-SYMBOLISATION/Archives",  # required
             # Icon class (if "type": "fontawesome"), or path to local image (if "type": "local")
             "icon": "fa-brands fa-square-github",
             # The type of image to be used (see below for details)
